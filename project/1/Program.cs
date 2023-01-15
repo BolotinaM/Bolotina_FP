48bb88c72c266c3a0db3da172d0eaf99fb30e661
Mariia <bolotina.23reg@yandex.ru>
2023-01-15 17:07:06 +0300

    Задали метод печати массива

--- project/1/Program.cs
+++ project/1/Program.cs
@@ -20,3 +20,11 @@ void SecondMass(string[] array1, string[] array2)
     }
 }
 
+void PrintMass(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
